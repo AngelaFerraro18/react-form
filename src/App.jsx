8f9901530f2d49516b16fa773561a32979a485f1
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-04-09 14:18:54 +0200

    feat: import articles and create articles list

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+//importo l'array come modulo
+import articles from "./data/articoli"
+
+
 
 function App() {
 
 
   return (
     <>
-
+      <h1>Benvenuto allo shop</h1>
+      <ul>
+        {articles.map(article => <li key={article.id}>{article.title}</li>)}
+      </ul>
     </>
   )
 }
