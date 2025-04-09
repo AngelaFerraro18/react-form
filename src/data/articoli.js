97ffe27e5bf87214adee424b4d5067313689fcd4
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-04-09 14:17:49 +0200

    feat: create data/articoli.js which contains articles list

---
+++ src/data/articoli.js
@@ -0,0 +1,26 @@
+const articles = [
+    {
+        id: '1',
+        title: 'Maglietta'
+    },
+    {
+        id: '2',
+        title: 'Gonna'
+    },
+    {
+        id: '3',
+        title: 'Jeans'
+    },
+    {
+        id: '4',
+        title: 'Pantalone'
+    },
+    {
+        id: '5',
+        title: 'Scarpe'
+    }
+]
+
+
+//esporto
+export default articles;
